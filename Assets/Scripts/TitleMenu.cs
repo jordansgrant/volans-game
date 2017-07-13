d1cf86259eeee786d68b3239d6899b3354a96356
jordansgrant <jordans.grant@gmail.com>
2017-07-12 21:21:55 -0400

    Initial commit, Started Title Menu and collected some assets for space ships, asteroids, and blank space backgrounds

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleMenu : MonoBehaviour {
+
+    public Button newGame,
+                  continueGame,
+                  exitGame;
+
+	// Use this for initialization
+    void Start ()
+    {
+        Button btn = newGame.GetComponent<Button>();
+        btn.onClick.AddListener(DoNewGame);
+
+        btn = continueGame.GetComponent<Button>();
+        btn.onClick.AddListener(DoContinueGame);
+
+        btn = exitGame.GetComponent<Button>();
+        btn.onClick.AddListener(DoExitGame);
+    }
+	
+	// Update is called once per frame
+    void Update ()
+    {
+	   	
+    }
+
+    void DoNewGame()
+    {
+        Debug.Log("New Game!");
+    }
+
+    void DoContinueGame()
+    {
+        Debug.Log("Continue Game!");
+    }
+
+    void DoExitGame()
+    {
+        Application.Quit();
+    }
+}
