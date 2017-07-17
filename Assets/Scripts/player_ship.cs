79486183902ecf10ee13390db642a478d0a5212d
jordansgrant <jordans.grant@gmail.com>
2017-07-17 16:02:45 -0400

    Added combat scene and ship movement.

--- Assets/Scripts/player_ship.cs
+++ Assets/Scripts/player_ship.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class player_ship : MonoBehaviour {
+public class player_ship : MonoBehaviour
+{
+    public int armor;
+    public int acceleration;
+    public int maxVelocity;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float rotation;
+    public Rigidbody2D player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        player = GetComponent<Rigidbody2D>();
+     
+        acceleration = 1000;
+        maxVelocity = 600;
+        rotation = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        bool isVerticalPressed = Input.GetButton("Vertical");
+
+        transform.Rotate(new Vector3(0, 0, horizontal * Time.deltaTime * rotation));
+
+        if (isVerticalPressed)
+        {
+            float accelCoefficient = 1.0f - (player.velocity.magnitude / maxVelocity);
+            player.AddForce(transform.up * Time.deltaTime * acceleration * accelCoefficient);
+        }
+    }
+
 }
