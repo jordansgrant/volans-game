78cb60337892a3501a47d4a13b4fa512ce029bf3
travisen <travis.nykaza@gmail.com>
2017-07-17 19:06:45 -0400

    Fix merge conflict in player_ship script

--- Assets/Scripts/Ships/player_ship.cs
+++ Assets/Scripts/Ships/player_ship.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Assets/Scripts/Collidables/player_ship.cs
 ﻿using UnityEngine;
 
 public class player_ship : MonoBehaviour
@@ -7,8 +6,12 @@ public class player_ship : MonoBehaviour
     public int acceleration;
     public int maxVelocity;
 
+    public int bulletSpeed;
+
     public float rotation;
     public Rigidbody2D player;
+
+    public GameObject bullet;
 
     // Use this for initialization
     void Start()
@@ -44,54 +47,6 @@ public class player_ship : MonoBehaviour
             float accelCoefficient = 1.0f - (player.velocity.magnitude / maxVelocity);
             player.AddForce(transform.up * Time.deltaTime * acceleration * accelCoefficient);
         }
-    }
-
-}
-=======
-﻿using UnityEngine;
-
-public class player_ship : MonoBehaviour
-{
-    public int armor;
-    public int acceleration;
-    public int maxVelocity;
-
-    public int bulletSpeed;
-
-    public float rotation;
-    public Rigidbody2D player;
-
-    public GameObject bullet;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    void Awake()
-    {
-        player = GetComponent<Rigidbody2D>();
-     
-        acceleration = 1000;
-        maxVelocity = 600;
-        rotation = 200;
-        bulletSpeed = 1500;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        bool isVerticalPressed = Input.GetButton("Vertical");
-
-        transform.Rotate(new Vector3(0, 0, horizontal * Time.deltaTime * rotation));
-
-        if (isVerticalPressed)
-        {
-            float accelCoefficient = 1.0f - (player.velocity.magnitude / maxVelocity);
-            player.AddForce(transform.up * Time.deltaTime * acceleration * accelCoefficient);
-        }
 
         if (Input.GetKeyDown("space"))
         {
@@ -100,4 +55,3 @@ public class player_ship : MonoBehaviour
     }
 
 }
->>>>>>> ecd734951fce8255e51a451371569177f0192b20:Assets/Scripts/player_ship.cs
