f91274dc2b2c9ed2631dad63fb43f39762b83ffa
jordansgrant <jordans.grant@gmail.com>
2017-07-20 01:40:46 -0400

    Added asteroids to Hostile encounter. Added collision effects for projectiles hitting asteroids. Added ship to asteroid collisions. Player now destroyed when too many hits are taken

--- Assets/Scripts/hostile_encounter.cs
+++ Assets/Scripts/hostile_encounter.cs
@@ -4,26 +4,97 @@ using UnityEngine;
 
 public class hostile_encounter : MonoBehaviour {
 
-    public GameObject player;
+    // Enemy Ship
     public GameObject enemy;
 
+    // player ship
+    public GameObject player;
     public GameObject playerSpawn;
-
     public string ship_type;
+
+    // Collidables
+    public Dictionary<string, List<GameObject>> asteroids;
+    public string asteroid_type;
+
+    private List<PolygonCollider2D> colliders;
 
     void Awake()
     {
-        ship_type = "battleship";
+        // Instantiate Data Storage
+        asteroids = new Dictionary<string, List<GameObject>>();
+        colliders = new List<PolygonCollider2D>();
+
+        ship_type = "cruiser";
         player = (GameObject)Resources.Load(@"Ships/" + ship_type);
+
+        // Setup Collidable prefabs
+        asteroids.Add("Brown", new List<GameObject>());
+        asteroids.Add("Grey", new List<GameObject>());
+
+        foreach (var size in new string[3] { "Large", "Med", "Small" })
+        {
+            foreach (var key in asteroids.Keys)
+            {
+                asteroids[key].Add(Resources.Load(@"Collidables\Asteroid_" +
+                                                  size + "_" + key) as GameObject);
+            }
+        }
     }
     // Use this for initialization
     void Start () {
+        // Spawn Player
         playerSpawn = GameObject.Find("PlayerSpawn");
         player = Instantiate(player, playerSpawn.transform.position, playerSpawn.transform.rotation);
-	}
+
+        colliders.Add(player.GetComponent<PolygonCollider2D>());
+
+        // Spawn asteroids
+        asteroid_type = "Brown";
+        System.Random rand = new System.Random(System.DateTime.Now.Millisecond);
+        int num_asteroids = rand.Next(1, 5);
+
+        int count = 0;
+        do
+        {
+            if (randomAsteroidSpawn())
+                count++;
+
+        } while (count < num_asteroids);
+
+    }
 
     // Update is called once per frame
     void Update () {
 		
-	}
+    }
+
+    private bool randomAsteroidSpawn()
+    {
+        GameObject asteroid;
+
+        float x = Random.Range(-13.0f, 13.0f);
+        float y = Random.Range(-9.0f, 9.0f);
+
+        asteroid = Instantiate(asteroids[asteroid_type][0], new Vector2(x, y), Quaternion.identity);
+        PolygonCollider2D collider = asteroid.GetComponent<PolygonCollider2D>();
+
+        if (collisionDetected(collider))
+        {
+            Destroy(asteroid);
+            return false;
+        }
+        ((Asteroid)asteroid.GetComponent("Asteroid")).size = 0;
+        colliders.Add(collider);
+        return true;
+    }
+
+    private bool collisionDetected(PolygonCollider2D spawn)
+    {
+        foreach (PolygonCollider2D col in colliders)
+        {
+            if (col.bounds.Intersects(spawn.bounds))
+                return true;
+        }
+        return false;
+    }
 }
