0d07d5fa0021c2defd562169cd5889b956c3edb2
travisen <travis.nykaza@gmail.com>
2017-08-02 21:11:19 -0400

    Add solar system gui elements and begin notifications on actions

--- Assets/Scripts/ShipFittingManager.cs
+++ Assets/Scripts/ShipFittingManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class ShipFittingManager : MonoBehaviour {
     
     public PlayerInventory inventory;
-    public Button SolarSystem;
+    public Button solarSystem;
 
     //Load Player's current inventory
     public void loadInventory()
@@ -17,20 +19,29 @@ public class ShipFittingManager : MonoBehaviour {
     void awake()
     {
         inventory = gameObject.GetComponents<PlayerInventory>()[0];
+
     }
 
     void start()
     {
-        print("Hello");
+        Button btn = solarSystem.GetComponent<Button>();
+        btn.onClick.AddListener(DoSelectSolarSystem);
+
         loadInventory();
     }
+
+    void DoSelectSolarSystem()
+    {
+        Debug.Log("SOLAR SYSTEM BTN");
+        SceneManager.LoadScene("SolarSystem");
+    }
     //Move item from inventory to ship
-        //Remove item from inventory
-        //Add to ship
-    
+    //Remove item from inventory
+    //Add to ship
+
     //Remove item from ship and into inventory
-        //remove item from ship
-        //add to inventory
-    
+    //remove item from ship
+    //add to inventory
+
     //Trash item from inventory
 }
---
+++ Assets/Scripts/SolarSystemGUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SolarSystemGUI : MonoBehaviour {
+
+    public Button mainMenu,
+                  shipFitting;
+
+    public GameObject notificationPanel;
+    public Text notification;
+
+    public static SolarSystemGUI instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this) 
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Button btn = mainMenu.GetComponent<Button>();
+        btn.onClick.AddListener(DoSelectMainMenu);
+
+        btn = shipFitting.GetComponent<Button>();
+        btn.onClick.AddListener(DoShipInventory);
+
+    }
+
+    void DoSelectMainMenu()
+    {
+        Debug.Log("Main menu!");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void DoShipInventory()
+    {
+        SceneManager.LoadScene("PlayerInventory");
+    }
+
+    public void DisplayNotification(string str)
+    {
+        notification.text = str;
+    }
+}
