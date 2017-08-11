ca307d344e6fecccb0c86a5d4408d2f2053fd561
travisen <travis.nykaza@gmail.com>
2017-08-11 14:44:00 -0400

    Can add from inventory to ship

--- Assets/Scripts/FittingManager.cs
+++ Assets/Scripts/FittingManager.cs
@@ -55,19 +55,38 @@ public class FittingManager : MonoBehaviour
             module = Resources.Load<GameObject>(@"Modules\" + mod) as GameObject;
             print(module);
             modules.Add(module);
-            path = "InventorySlot" + i + "/Button";
+            path = "InventorySlot" + i + "/InvButton" + i;
 
             GameObject.Find(path).GetComponent<Image>().sprite = module.GetComponentInChildren<Image>().sprite;
             GameObject.Find(path).GetComponent<Image>().enabled = true;
+            GameObject.Find(path).GetComponentInChildren<Text>().text = mod;
 
             i++;
         }
     }
 
+
+    void InitializeInvButtons()
+    {
+        
+        for(int i = 0; i < 6; i++)
+        {
+            string path = "InvButton" + i;
+            print(path);
+            Button btn = GameObject.Find(path).GetComponent<Button>();
+            btn.onClick.AddListener(delegate { AddToFit(path); });
+        }
+
+    }
+
     void Awake()
     {
+        //Solar system button
         Button btn = SolarSys.GetComponent<Button>();
         btn.onClick.AddListener(BackToSolar);
+
+        //Module buttons
+        InitializeInvButtons();
 
         modules = new List<GameObject>();
 
@@ -97,6 +116,42 @@ public class FittingManager : MonoBehaviour
 
         SceneManager.LoadScene("SolarSystem");
     }
+
+    void AddToFit(string current)
+    {
+
+        GameObject.Find(current).GetComponent<Image>().enabled = false;
+        string module = GameObject.Find(current).GetComponentInChildren<Text>().text;
+        GameObject.Find(current).GetComponentInChildren<Text>().text = "";
+        GameManager.game.pData.moduleInventory.Add(module);
+
+        DrawAddedToFitting(module);
+
+        print(GameObject.Find(current).GetComponent<Image>());
+
+    }
+
+    bool DrawAddedToFitting(string module)
+    {
+        print(GameObject.Find("Modules"));
+        GameObject newMod = Resources.Load<GameObject>(@"Modules\" + module) as GameObject;
+        
+        //4 should eventually be replaced by number of slots on a ship
+        for(int i = 0; i < 4; i++)
+        {
+            if(GameObject.Find("ModSlot" + i).GetComponentInChildren<Button>().GetComponent<Image>().sprite.name == "UISprite")
+            {
+                //print(GameObject.Find("ModSlot" + i).GetComponentInChildren<Button>().GetComponent<Image>().sprite);
+                GameObject.Find("ModSlot" + i).GetComponentInChildren<Button>().GetComponent<Image>().sprite = newMod.GetComponentInChildren<Image>().sprite;
+                GameObject.Find("ModSlot" + i).GetComponentInChildren<Button>().GetComponent<Image>().enabled = true;
+                return true;
+            }
+        }
+
+        //False if no space to add object
+        return false;
+    }
+    
     //Move item from inventory to ship
     //Remove item from inventory
     //Add to ship
