3fa2fbb4acd2ffa8eada762017a0d636c89a8088
jordansgrant <jordans.grant@gmail.com>
2017-08-15 16:05:05 -0600

    Fixed the TitleMusic class. It previously would play the default music if you started on a Scene that is supposed to play something else

--- Assets/Scripts/Music/TitleMusic.cs
+++ Assets/Scripts/Music/TitleMusic.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Events;
 
 [System.Serializable]
 public class TitleMusic : MonoBehaviour {
     public static TitleMusic music;
     public List<AudioClip> audioClips;
     public bool lastSongWasTitle;
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start() {
         // Set Don't destroy if this is the first time GameManager is Instantiated
         if (music == null)
         {
@@ -22,17 +23,23 @@ public class TitleMusic : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-        
-    }
-	
 
-    void OnLevelWasLoaded(int level)
-    {
         AudioSource audio = GetComponent<AudioSource>(); ;
         MusicInfo m = GameObject.Find("Main Camera").GetComponent<MusicInfo>();
+        audio.clip = audioClips[m.songIndex];
+        audio.Play();
 
+        SceneManager.activeSceneChanged += SceneChanged;
+    }
+    
+    void SceneChanged(Scene prevScene, Scene activeScene)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        MusicInfo m = GameObject.Find("Main Camera").GetComponent<MusicInfo>();
+        Debug.Log("Changing Song: " + m.songIndex + " " + m.isTitleSong);
         if (!m.isTitleSong || !lastSongWasTitle)
         {
+            Debug.Log("Changing Song: " + m.songIndex + " " + m.isTitleSong);
             audio.Stop();
             audio.clip = audioClips[m.songIndex];
             audio.Play();
